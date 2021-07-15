8afedb98563250b6c69fdb993d9a724e93d1b464
Jory Vardas <joryvardas@gmail.com>
2021-07-15 00:17:37 -0600

    Fix registerCommandlineOptions callsite
    The callsite updated in commit c9cc14b4de is incorrect, and needs
    to be fixed.

--- src/app/app.cpp
+++ src/app/app.cpp
@@ -1,6 +1,7 @@
 #include "../config/config.h"
 #include "../database/database.h"
 #include "archiver.h"
+#include "commandline_options.hpp"
 #include "common.h"
 #include <cxxopts.hpp>
 #include <iostream>
@@ -9,7 +10,9 @@
 #include <vector>
 
 int main(int argc, const char* argv[]) {
-    auto arguments = registerCommandlineOptions(CArray{argv, argc});
+    auto registeredOptions = registerCommandlineOptions();
+    auto options = parseCommandlineOptions(
+        registeredOptions, CArray{argv, static_cast<std::size_t>(argc)});
 
     std::shared_ptr<Config> defaultConfig =
         std::make_shared<Config>("settings.json"_path);
