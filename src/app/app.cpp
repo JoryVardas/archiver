7dc752cc285aedc6382578006e90f9feda8793c1
Jory Vardas <joryvardas@gmail.com>
2021-07-15 01:10:34 -0600

    Remove old/unused code from main

--- src/app/app.cpp
+++ src/app/app.cpp
@@ -16,35 +16,6 @@ int main(int argc, const char* argv[]) {
 
     std::shared_ptr<Config> defaultConfig =
         std::make_shared<Config>("settings.json"_path);
-    // Database database{DatabaseConnectionParameters{
-    //     defaultConfig.database.location.host,
-    //     defaultConfig.database.location.port,
-    //     defaultConfig.database.user,
-    //     defaultConfig.database.password,
-    //     defaultConfig.database.location.schema,
-    // }};
-
-    if (arguments.count("archive")) {
-        // Archiver archiver(database);
-
-        auto archiveLocations =
-            arguments["archive"].as<std::vector<std::filesystem::path>>();
-        // for (auto location : archiveLocations) {
-        //     if (arguments.count("prefix")) {
-        //         auto prefix =
-        //         arguments["prefix"].as<std::filesystem::path>(); if
-        //         (location.starts_with(prefix)) {
-        //             location.erase(0, prefix.length());
-        //         }
-        //     }
-        //     archiver.addPathToArchive(std::filesystem::path(
-        //         location, std::filesystem::path::format::generic_format));
-        // }
-
-        // archiver.archive();
-    };
-    if (arguments.count("dearchive") > 0) {
-    };
 
     return EXIT_SUCCESS;
 }
