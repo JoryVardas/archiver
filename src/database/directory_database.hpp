952b0d82f5afaf8d252c68d38c4792582134f78a
Jory Vardas <joryvardas@gmail.com>
2021-10-06 11:26:29 -0600

    refactor(database:directory): change string references to string_view

--- src/database/directory_database.hpp
+++ src/database/directory_database.hpp
@@ -2,15 +2,17 @@
 #define ARCHIVER_DIRECTORY_DATABASE_HPP
 
 #include "../app/directory.h"
+#include <string_view>
 
 class DirectoryDatabase {
   public:
     virtual std::optional<ArchivedDirectory>
     getDirectory(DirectoryID id) const abstract;
     virtual std::optional<ArchivedDirectory>
-    getDirectory(DirectoryID parent, const std::string& name) const abstract;
-    virtual ArchivedDirectory addNewDirectory(DirectoryID parent,
-                                              const std::string& name) abstract;
+    getDirectory(DirectoryID parent,
+                 const std::string_view name) const abstract;
+    virtual ArchivedDirectory
+    addNewDirectory(DirectoryID parent, const std::string_view name) abstract;
 };
 
 #endif
