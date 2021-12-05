b771308e1a208a9f2199f37d357699c84338002b
Jory Vardas <joryvardas@gmail.com>
2021-12-04 19:40:41 -0700

    feat(database): add staged_file and staged_directory tables
    Add stage_file and staged_directory tables to the make.sql file.
    
    Also reformat the file.

--- src/database/make.sql
+++ src/database/make.sql
@@ -1,53 +1,85 @@
-DROP DATABASE IF EXISTS `archiver`;
+DROP
+DATABASE IF EXISTS `archiver`;
 
-CREATE DATABASE `archiver`;
+CREATE
+DATABASE `archiver`;
 
-USE `archiver`;
+USE
+`archiver`;
 
-CREATE TABLE `directory` (
-	`id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(1024) NOT NULL,
-	`parent_id` BIGINT UNSIGNED NOT NULL,
-	PRIMARY KEY (`id`),
-	FOREIGN KEY (`parent_id`) REFERENCES `directory`(`id`)
+CREATE TABLE `directory`
+(
+    `id`        BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name`      VARCHAR(1024) NOT NULL,
+    `parent_id` BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`parent_id`) REFERENCES `directory` (`id`)
 );
 
-INSERT INTO `directory` (`name`, `parent_id`) VALUES ("/", 1);
+INSERT INTO `directory` (`name`, `parent_id`)
+VALUES ("/", 1);
 
-CREATE TABLE `file` (
-	`id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(1024) NOT NULL,
-	`parent_id` BIGINT UNSIGNED NOT NULL,
-	PRIMARY KEY (`id`),
-	FOREIGN KEY (`parent_id`) REFERENCES `directory`(`id`)
+CREATE TABLE `file`
+(
+    `id`        BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name`      VARCHAR(1024) NOT NULL,
+    `parent_id` BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`parent_id`) REFERENCES `directory` (`id`)
 );
 
-CREATE TABLE `archive` (
-	`id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-	`contents` VARCHAR(255) NOT NULL,
-	PRIMARY KEY (`id`)
+CREATE TABLE `archive`
+(
+    `id`       BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `contents` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`)
 );
 
-INSERT INTO `archive` (`contents`) VALUES ("<SINGLE>");
+INSERT INTO `archive` (`contents`)
+VALUES ("<SINGLE>");
 
-CREATE TABLE `file_revision` (
-	`file_id` BIGINT UNSIGNED NOT NULL,
-	`blake2b` BINARY(64) NOT NULL,
-	`sha3` BINARY(64) NOT NULL,
-	`size` BIGINT UNSIGNED NOT NULL,
-	`archive_time` DATETIME(2) NOT NULL,
-	`archive` BIGINT UNSIGNED NOT NULL,
-	PRIMARY KEY (`file_id`, `archive_time`),
-	FOREIGN KEY (`file_id`) REFERENCES `file`(`id`),
-	FOREIGN KEY (`archive`) REFERENCES `archive`(`id`)
+CREATE TABLE `file_revision`
+(
+    `revision_id`  BIGINT UNSIGNED NOT NULL AUTOINCREMENT,
+    `file_id`      BIGINT UNSIGNED NOT NULL,
+    `blake2b`      CHAR(88) NOT NULL,
+    `sha3`         CHAR(88) NOT NULL,
+    `size`         BIGINT UNSIGNED NOT NULL,
+    `archive_time` DATETIME(2) NOT NULL,
+    `archive`      BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (`revision_id`),
+    FOREIGN KEY (`file_id`) REFERENCES `file` (`id`),
+    FOREIGN KEY (`archive`) REFERENCES `archive` (`id`)
 );
 
-CREATE TABLE `file_duplicate` (
-	`file_id` BIGINT UNSIGNED NOT NULL,
-	`archive_time` DATETIME(2) NOT NULL,
-	`duplicate_file_id` BIGINT UNSIGNED NOT NULL,
-	`duplicate_file_archive_time` DATETIME(2) NOT NULL,
-	PRIMARY KEY (`file_id`, `archive_time`),
-	FOREIGN KEY (`file_id`) REFERENCES `file`(`id`),
-	FOREIGN KEY (`duplicate_file_id`, `duplicate_file_archive_time`) REFERENCES `file_revision`(`file_id`, `archive_time`)
+CREATE TABLE `file_duplicate`
+(
+    `id`                    BIGINT UNSIGNED NOT NULL AUTOINCREMENT,
+    `file_id`               BIGINT UNSIGNED NOT NULL,
+    `archive_time`          DATETIME(2) NOT NULL,
+    `duplicate_revision_id` BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`file_id`) REFERENCES `file` (`id`),
+    FOREIGN KEY (`duplicate_revision_id`) REFERENCES `file_revision` (`revision_id`)
+);
+
+CREATE TABLE `staged_directory`
+(
+    `directory_id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name`         VARCHAR(1024) NOT NULL,
+    `parent_id`    BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`parent_id`) REFERENCES `staged_directory` (`id`)
+);
+
+CREATE TABLE `staged_file`
+(
+    `id`        BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name`      VARCHAR(1024) NOT NULL,
+    `parent_id` BIGINT UNSIGNED NOT NULL,
+    `blake2b`   CHAR(88)      NOT NULL,
+    `sha3`      CHAR(88)      NOT NULL,
+    `size`      BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`parent_id`) REFERENCES `directory` (`id`)
 );
