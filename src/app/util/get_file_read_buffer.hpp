bb3ae7c844626df98075ab3b122c37f2e3b75c49
Jory Vardas <joryvardas@gmail.com>
2021-12-19 14:20:16 -0700

    feat(util): add function to get a file read buffer based on config

---
+++ src/app/util/get_file_read_buffer.hpp
@@ -0,0 +1,27 @@
+#ifndef ARCHIVER_GET_FILE_READ_BUFFER_HPP
+#define ARCHIVER_GET_FILE_READ_BUFFER_HPP
+
+#include "../../config/config.h"
+#include "../common.h"
+#include <memory>
+
+namespace {
+std::pair<std::unique_ptr<uint8_t[]>, Size>
+getFileReadBuffer(const Config& config) {
+  for (auto size : config.general.fileReadSizes) {
+    try {
+      auto data = std::make_unique<uint8_t[]>(size);
+      return std::make_pair(std::move(data), size);
+    } catch (const std::bad_alloc&) {
+      // If there was a bad alloc, try the next available size
+      continue;
+    }
+  }
+  // If none of the sizes could be allocated then throw an error.
+  throw std::runtime_error(fmt::format(
+    "Unable to allocate a file read buffer of any of the given sizes: {}"s,
+    fmt::join(config.general.fileReadSizes, ","s)));
+}
+}
+
+#endif
