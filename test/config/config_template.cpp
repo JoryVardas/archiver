61bb9582c9966a216cbe835f92aa89289d9ad2b7
Jory Vardas <joryvardas@gmail.com>
2022-02-01 15:27:24 -0700

    fix(test): fix paths of config files for config tests
    The config files are in test/config but the test application is under
    test, as such the config tests couldn't find the required config files.

--- test/config/config_template.cpp
+++ test/config/config_template.cpp
@@ -2,8 +2,8 @@
 #include <src/config/config.h>
 
 TEST_CASE("Loading a complete config file", "[config]") {
-  REQUIRE_NOTHROW(Config("./test_config.json"));
-  Config config("./test_config.json");
+  REQUIRE_NOTHROW(Config("./config/test_config.json"));
+  Config config("./config/test_config.json");
 
   REQUIRE(config.archive.single_archive_size == 104857600);
 
@@ -27,36 +27,40 @@ TEST_CASE("Loading a complete config file", "[config]") {
 }
 
 TEST_CASE("Loading a config file missing a single value", "[config]") {
-  REQUIRE_THROWS_AS(Config("./test_config_incomplete1.json"), ConfigError);
+  REQUIRE_THROWS_AS(Config("./config/test_config_incomplete1.json"),
+                    ConfigError);
   REQUIRE_THROWS_WITH(
-    Config("./test_config_incomplete1.json"),
+    Config("./config/test_config_incomplete1.json"),
     "Config file is missing a required entry \"stager/stage_directory\"");
 }
 
 TEST_CASE("Loading a config file missing an entire section", "[config]") {
-  REQUIRE_THROWS_AS(Config("./test_config_incomplete2.json"), ConfigError);
-  REQUIRE_THROWS_WITH(Config("./test_config_incomplete2.json"),
+  REQUIRE_THROWS_AS(Config("./config/test_config_incomplete2.json"),
+                    ConfigError);
+  REQUIRE_THROWS_WITH(Config("./config/test_config_incomplete2.json"),
                       "Config file is missing a required entry \"aws\"");
 }
 
 using Catch::Matchers::StartsWith;
 
 TEST_CASE("Loading an empty config file", "[config]") {
-  REQUIRE_THROWS_AS(Config("./test_config_empty.json"), ConfigError);
+  REQUIRE_THROWS_AS(Config("./config/test_config_empty.json"), ConfigError);
   REQUIRE_THROWS_WITH(
-    Config("./test_config_empty.json"),
+    Config("./config/test_config_empty.json"),
     StartsWith("The following error occured while reading the "
                "configuration file:\n\t"));
 }
 
 TEST_CASE("Loading a config file which doesn't exist", "[config]") {
-  REQUIRE_THROWS_AS(Config("./test_config_non_existent.json"), ConfigError);
-  REQUIRE_THROWS_WITH(Config("./test_config_non_existent.json"),
+  REQUIRE_THROWS_AS(Config("./config/test_config_non_existent.json"),
+                    ConfigError);
+  REQUIRE_THROWS_WITH(Config("./config/test_config_non_existent.json"),
                       StartsWith("Could not open the configuration file"));
 }
 
 TEST_CASE("Loading a config file with an incorrect value type", "[config]") {
-  REQUIRE_THROWS_AS(Config("./test_config_incorrect_type.json"), ConfigError);
-  REQUIRE_THROWS_WITH(Config("./test_config_incorrect_type.json"),
+  REQUIRE_THROWS_AS(Config("./config/test_config_incorrect_type.json"),
+                    ConfigError);
+  REQUIRE_THROWS_WITH(Config("./config/test_config_incorrect_type.json"),
                       StartsWith("Config file has value of incorrect type"));
 }
