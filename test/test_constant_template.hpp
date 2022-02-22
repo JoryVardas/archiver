98b6c9dd50dcec9d13430b62d8f35327f0fff2c3
Jory Vardas <joryvardas@gmail.com>
2022-02-22 16:31:09 -0700

    build(test): generate random test files
    Since storing test files in git would take a lot of space and there are
    no dependencies on their contents, generate them from random data each
    during project configuration. The size and hash of the random files
    needs to be known so get that data and store it in a header file.
    
    This will currently only work on linux based machines and there is
    currently no test to ensure that the required commands are available.

---
+++ test/test_constant_template.hpp
@@ -0,0 +1,34 @@
+#ifndef ARCHIVER_TESTCONSTANTTEMPLATE_HPP
+#define ARCHIVER_TESTCONSTANTTEMPLATE_HPP
+#include <string>
+
+namespace ArchiverTest {
+using namespace std::string_literals;
+namespace TestData1 {
+const std::string hash =
+  "${ARCHIVER_TEST_TEST_DATA_1_SHA}${ARCHIVER_TEST_TEST_DATA_1_BLAKE2B}"s;
+const uint64_t size = ${ARCHIVER_TEST_TEST_DATA_1_SIZE};
+}
+namespace TestDataSingleExact {
+const std::string hash =
+  "${ARCHIVER_TEST_TEST_DATA_SINGLE_EXACT_SHA}${ARCHIVER_TEST_TEST_DATA_SINGLE_EXACT_BLAKE2B}"s;
+const uint64_t size = ${ARCHIVER_TEST_TEST_DATA_SINGLE_EXACT_SIZE};
+}
+namespace TestDataSingle {
+const std::string hash =
+  "${ARCHIVER_TEST_TEST_DATA_SINGLE_SHA}${ARCHIVER_TEST_TEST_DATA_SINGLE_BLAKE2B}"s;
+const uint64_t size = ${ARCHIVER_TEST_TEST_DATA_SINGLE_SIZE};
+}
+namespace TestDataNotSingle {
+const std::string hash =
+  "${ARCHIVER_TEST_TEST_DATA_NOT_SINGLE_SHA}${ARCHIVER_TEST_TEST_DATA_NOT_SINGLE_BLAKE2B}"s;
+const uint64_t size = ${ARCHIVER_TEST_TEST_DATA_NOT_SINGLE_SIZE};
+}
+namespace TestDataCopy {
+const std::string hash =
+  "${ARCHIVER_TEST_TEST_DATA_COPY_SHA}${ARCHIVER_TEST_TEST_DATA_COPY_BLAKE2B}"s;
+const uint64_t size = ${ARCHIVER_TEST_TEST_DATA_COPY_SIZE};
+}
+}
+
+#endif
