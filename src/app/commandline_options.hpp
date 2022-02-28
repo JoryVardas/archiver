77e37e638972b36adceb36056217f99f1d310bdf
Jory Vardas <joryvardas@gmail.com>
2022-02-28 15:20:55 -0700

    fix: add missing constructors and destructors
    Since classes derived from cxxsubs::IOptions have virtual functions a
    virtual destructor is required. Add copy/move constructors/assignment
    functions needed to follow Core Guidelines C.21.

--- src/app/commandline_options.hpp
+++ src/app/commandline_options.hpp
@@ -9,6 +9,12 @@ _make_exception_(CommandValidateException);
 class StageCommand : public cxxsubs::IOptions {
 public:
   StageCommand();
+  StageCommand(const StageCommand&) = delete;
+  StageCommand(StageCommand&&) = delete;
+  virtual ~StageCommand() = default;
+
+  StageCommand& operator=(const StageCommand&) = delete;
+  StageCommand& operator=(StageCommand&&) = delete;
 
   int validate() override;
   int exec() override;
@@ -16,6 +22,12 @@ public:
 class ArchiveCommand : public cxxsubs::IOptions {
 public:
   ArchiveCommand();
+  ArchiveCommand(const ArchiveCommand&) = delete;
+  ArchiveCommand(ArchiveCommand&&) = delete;
+  virtual ~ArchiveCommand() = default;
+
+  ArchiveCommand& operator=(const ArchiveCommand&) = delete;
+  ArchiveCommand& operator=(ArchiveCommand&&) = delete;
 
   int validate() override;
   int exec() override;
@@ -23,6 +35,12 @@ public:
 class UploadCommand : public cxxsubs::IOptions {
 public:
   UploadCommand();
+  UploadCommand(const UploadCommand&) = delete;
+  UploadCommand(UploadCommand&&) = delete;
+  virtual ~UploadCommand() = default;
+
+  UploadCommand& operator=(const UploadCommand&) = delete;
+  UploadCommand& operator=(UploadCommand&&) = delete;
 
   int validate() override;
   int exec() override;
@@ -30,6 +48,12 @@ public:
 class DearchiveCommand : public cxxsubs::IOptions {
 public:
   DearchiveCommand();
+  DearchiveCommand(const DearchiveCommand&) = delete;
+  DearchiveCommand(DearchiveCommand&&) = delete;
+  virtual ~DearchiveCommand() = default;
+
+  DearchiveCommand& operator=(const DearchiveCommand&) = delete;
+  DearchiveCommand& operator=(DearchiveCommand&&) = delete;
 
   int validate() override;
   int exec() override;
